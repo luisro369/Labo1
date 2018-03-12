eb190e3b6b2cf5cb20af8a47827f709a93a2605a
luisro <luisro@lenovo-debian>
2018-03-12 12:46:25 -0600

    primer comentario

---
+++ app/src/main/java/com/luisro00005513/labo1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.luisro00005513.labo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //==============codigo==================
+        TextView first = (TextView)findViewById(R.id.txtViewFirst);
+        first.setText("holi perros!!!!!!!!!!!!!!");
+    }
+}
